74ff377395a88afce8ccdd4748bbdbd084425ab0
Mariano Epherra <marianoepherra@gmail.com>
2023-11-17 20:31:01 -0300

    cambios

--- Js/app.js
+++ Js/app.js
@@ -21,6 +21,68 @@ switch(inicioCuenta) {
         alert ('Lo siento no estas registrado');
         break;
 }
+let id = 0;
+let nombre = "";
+let precio = 0;
+
+const agregadoProductos = [];
+
+const Productos = [
+    { id: 1, nombre: 'hamburguesa clasica', precio: 500},
+    { id: 2, nombre: 'Pizza napo', precio: 700},
+    { id: 3, nombre: 'empanadas arabes', precio: 600},
+
+];
+
+class Producto {
+    constructor(){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+
+
+// console.log(Productos);
+
+function ingresarProducto(){
+    let nuevoProducto;
+
+    id = ++ id;
+    nombre = prompt(`Introduzca el nombre del producto`);
+    precio = prompt(`introduzca el precio del prodcuto`);
+    nuevoProducto = new Producto (id, nombre, precio);
+    console.log(nuevoProducto);
+    Productos.push(nuevoProducto);
+
+
+}
+
+localStorage.setItem('Productos', JSON.stringify(Productos));
+const recuperarProductos = JSON.parse(localStorage.getItem('Productos'));
+console.log(recuperarProductos);
+
+localStorage.setItem('Agregar', JSON.stringify(nuevoProducto));
+const miProducto = JSON.parse(localStorage.getItem('Agregar'));
+console.log(miProducto);
+
+
+
+
+ingresarProducto();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
