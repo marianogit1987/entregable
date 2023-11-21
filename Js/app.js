9c7082411df9ae80ef0353bf28b12e306e9a0b39
Mariano Epherra <marianoepherra@gmail.com>
2023-11-21 12:33:50 -0300

    Dom

--- Js/app.js
+++ Js/app.js
@@ -1,42 +1,44 @@
-let inicioCuenta; confirm ('Hola, eres un Usuario Registrado?')
+const carrito = document.getElementsByClassName("Agregar al carrito")
+let agregadoProductos = [];
 
 
-do{
-    inicioCuenta= prompt('Ingrese Nombre Ususario')
-} while(inicioCuenta == '' ||  inicioCuenta == null);
-
-
-switch(inicioCuenta) {
-
-    
-    case 'mariano':
-        alert ('Bienvenido MARIANO');
-        break;
-
-    case 'marisa':
-        alert ('Bienvenido MARISA');
-        break;
-
-    default:
-        alert ('Lo siento no estas registrado');
-        break;
+function cargaralCarrito  () {
+    carrito.addEventListener('click', "Agregar al carrito")
+    console.log(cargaralCarrito)
 }
-let id = 0;
-let nombre = "";
-let precio = 0;
 
-const agregadoProductos = [];
 
-const Productos = [
-    { id: 1, nombre: 'hamburguesa clasica', precio: 500},
-    { id: 2, nombre: 'Pizza napo', precio: 700},
-    { id: 3, nombre: 'empanadas arabes', precio: 600},
 
-];
+
+// let id = 0;
+// let nombre = "";
+// let precio = 0;
+
+
+
+// const Productos = [
+//     { id: 1, nombre: 'hamburguesa clasica', precio: 500},
+//     { id: 2, nombre: 'Pizza napo', precio: 700},
+//     { id: 3, nombre: 'empanadas arabes', precio: 600},
+
+// ];
+
+    // for(const producto of Productos) {
+    //     let contenedor = document.createElement("div");
+    //     contenedor.innerHTML = `<h3> ID: ${producto.id}</h3>
+    //                             <p> Producto: ${producto.nombre}</p>
+    //                             <b> $ ${producto.precio} </b>`;
+
+    //     document.body.appendChild(contenedor);
+
+    // }                           
+
+
 
 class Producto {
-    constructor(){
+    constructor(id, imagen, nombre, precio){
         this.id = id;
+        this.imagen = imagen;
         this.nombre = nombre;
         this.precio = precio;
     }
@@ -48,38 +50,38 @@ class Producto {
 
 // console.log(Productos);
 
-function ingresarProducto(){
-    let nuevoProducto;
+// function ingresarProducto(){
+//     let nuevoProducto;
 
-    id = ++ id;
-    nombre = prompt(`Introduzca el nombre del producto`);
-    precio = prompt(`introduzca el precio del prodcuto`);
-    nuevoProducto = new Producto (id, nombre, precio);
-    console.log(nuevoProducto);
-    Productos.push(nuevoProducto);
+//     id = ++ id;
+//     nombre = prompt(`Introduzca el nombre del producto`);
+//     precio = prompt(`introduzca el precio del prodcuto`);
+//     nuevoProducto = new Producto (id, nombre, precio);
+//     console.log(nuevoProducto);
+//     Productos.push(nuevoProducto);
 
-    localStorage.setItem('Agregar', JSON.stringify(nuevoProducto));
-    const miProducto = JSON.parse(localStorage.getItem('Agregar'));
-    // console.log(miProducto);
+//     localStorage.setItem('Agregar', JSON.stringify(nuevoProducto));
+//     const miProducto = JSON.parse(localStorage.getItem('Agregar'));
+//     // console.log(miProducto);
 
-}
-
-
-
-
-
-localStorage.setItem('Productos', JSON.stringify(Productos));
-const recuperarProductos = JSON.parse(localStorage.getItem('Productos'));
-console.log(recuperarProductos);
-
-
-
-localStorage.removeItem('Agregar')
+// }
 
 
 
 
-ingresarProducto();
+
+// localStorage.setItem('Productos', JSON.stringify(Productos));
+// const recuperarProductos = JSON.parse(localStorage.getItem('Productos'));
+// console.log(recuperarProductos);
+
+
+
+// localStorage.removeItem('Agregar')
+
+
+
+
+// ingresarProducto();
 
 
 
