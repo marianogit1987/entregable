b21ec3dc4d59b33b2f82d55f7676c327e9eb2a9f
Mariano Epherra <marianoepherra@gmail.com>
2023-12-15 20:39:28 -0300

    ultima-parte

--- Js/app.js
+++ Js/app.js
@@ -1,17 +1,9 @@
-const containerProdutos = document.getElementById("container-Productos")
-let agregadoProductos = [];
-
-cargarEventos();
-//Trato de hacer un click en los diferentes container-Productos y solamente me hace click en el primer contenedor
-function cargarEventos(){
-    containerProdutos.addEventListener('click', agregarProducto);
-}
-
-function  agregarProducto(){
-    console.log('se hizo click');
-}
-
-
+const containerProduct = document.getElementById ("container-productos")
+const containerCart = document.querySelector(".modal-cart")
+const carrito = document.getElementById('carrito')
+const modal = document.getElementById('window-cart')
+const btnClose = document.getElementsByClassName('close')[0]
+let productosCarrito = [];
 
 class Producto {
     constructor(id, imagen, nombre, precio){
@@ -28,8 +20,131 @@ class Producto {
     }
 }
 
+cargarEventos();
+
+function cargarEventos(){
+    containerProduct.addEventListener('click', agregarProducto);
+
+    document,addEventListener('DOMContentLoaded', () => {
+        renderizarProductos();
+    });
+
+    carrito.onclick = function () {
+        modal.style.display = 'block';
+    };
+
+    btnClose.onclick = function() {
+        ocultarmodal();
+    };
+
+    window.onclick = function(e) {
+        if (e.target == modal){
+            ocultarmodal();
+        };
+    }
+}
 
 
+function ocultarmodal(){
+    modal.style.display = 'none';
+}
+
+function  agregarProducto(e){
+    e.preventDefault();
+
+    if (e.target.classList.contains("agregar-carrito")) {
+        const productoAgregado = e.target.parentElement;
+
+        leerDatosProducto(productoAgregado);
+    }
+}
+
+    function leerDatosProducto (producto) {
+        const datosProducto = new Producto(
+            parseInt(producto.querySelector('a').getAttribute('id')),
+            producto.querySelector('img').src,
+            producto.querySelector('h4').textContent,
+            Number(producto.querySelector('p').textContent.replace('$', '')));
+
+            datosProducto.obtenerTotal();
+
+           agregaralCarrito(datosProducto);
+    }
+
+    function agregaralCarrito(productoAgregar){
+        // console.log(productoAgregar);
+        // console.log(productosCarrito);
+
+        const existeEnCarrito = productosCarrito.some((producto) =>producto.id === productoAgregar.id);
+
+        if (existeEnCarrito) {
+            const productos = productosCarrito.map((producto) => {
+                if
+                (producto.id === productoAgregar.id) {
+                    producto.cantidad++;
+                    producto.subtotal = producto.precio * producto.cantidad;
+
+                    return producto;
+                } else {
+                    return producto;
+                }                
+            });
+
+            productosCarrito = productos;
+        } else{
+            productosCarrito.push(productoAgregar);
+
+        }
+        console.log(productosCarrito);
+        mostrarProductosCarrito();
+}
+
+function mostrarProductosCarrito() {
+
+    limpiarHtml();
+
+    productosCarrito.forEach((producto) => {
+        const {id, imagen, nombre, precio, cantidad, subtotal} = producto;
+
+        const div = document.createElement('div')
+        div.classList.add('contenedor-producto');
+        div.innerHTML = `
+                            <img src="${imagen}">
+                            <p>${nombre}</p>
+                            <p>$${precio}</p>
+                            <p>${cantidad}</p>
+                            <p>$${subtotal}</p>
+                            <a href="#" class="eliminar-producto" id= "${id}"> X </a>
+                        `;
+        containerCart.append(div);
+    });
+}
+
+function limpiarHtml(){
+    while (containerCart.firstChild) {
+        containerCart.removeChild(containerCart.firstChild)
+    }
+}
+
+    function renderizarProductos(){
+        products.forEach((producto) =>{
+            const divCard = document.createElement('div');
+            divCard.classList.add('card');
+            divCard.innerHTML += `
+            <img src="../img/${producto.img}" alt="${producto.nombre}" />
+            <h4>${producto.nombre}</h4>
+            <p>$ ${producto.precio}</p>
+            <a id= ${producto.id} class='boton agregar-carrito' href= '#'>Agregar </a>
+            
+        `;
+
+        containerProduct.append(divCard);
+            
+                
+        });
+    }
+
+    
 
 
 
---
+++ Js/products.js
@@ -0,0 +1,43 @@
+const products = [
+    {
+        id:1,
+        nombre: 'Dean and Dennys',
+        precio: 2000,
+        img:'dean.jpg'
+    },
+    
+    {
+        id:2,
+        nombre: "Milwaukee",
+        precio: 2300,
+        img:"Milwaukee.jpg"
+    },
+
+    {
+        id:3,
+        nombre: "Big Kevin",
+        precio: 2500,
+        img:"Big Kevin.jpg"
+    },
+
+    {
+        id:4,
+        nombre: "Burger Americana",
+        precio: 2500,
+        img:"Burger Americana.jpg"
+    },
+
+    {
+        id:5,
+        nombre: "Veggie",
+        precio: 2600,
+        img:"Veggie.jpg"
+    },
+
+    {
+        id:6,
+        nombre: "Mexican",
+        precio: 2800,
+        img:"Mexican.jpg"
+    }
+]
